c3b9ec0fdfff08969f805965b6fbf983bb77ba92
YunusKaratepe <yunus.karatepe1699@gmail.com>
2021-08-15 12:19:07 +0300

    sss

--- data_analysis_tools.py
+++ data_analysis_tools.py
@@ -888,10 +888,63 @@ class ml:
                         self.means[i] = vector_mean(i_points)
 
 
+class Table:
+    def __init__(self, columns) -> None:
+        self.columns = columns
+        self.rows = []
 
+    def __repr__(self) -> str:
+        return str(self.columns) + "\n" + "\n".join(map(str, self.rows))
 
+    def insert(self, row_values):
+        if len(row_values) != len(self.columns):
+            raise TypeError('wrong number of elements')
+        row_dict = dict(zip(self.columns, row_values))
+        self.rows.append(row_dict)
 
+    def update(self, updates, predicate):
+        for row in self.rows:
+            if predicate(row):
+                for column, new_value in updates.items():
+                    row[column] = new_value
 
+    def delete(self, predicate=lambda row: True):
+        """deletes all rows matching predicate, if predicate is not given deletes all rows"""
+        self.rows = [row for row in self.rows if not(predicate(row))]
+
+    def select(self, keep_columns=None, additional_columns=None):
+
+        if keep_columns is None:
+            keep_columns = self.columns
+
+        if additional_columns is None:
+            additional_columns = {}
+
+        # new table for results
+        result_table = Table(keep_columns + list(additional_columns.keys()))
+    
+        for row in self.rows:
+            new_row = [row[column] for column in keep_columns]
+            for _, calculation in additional_columns.items():
+                new_row.append(calculation(row))
+            result_table.insert(new_row)
+            
+        return result_table
+
+    def where(self, predicate=lambda row: True):
+        """return only the rows that satisfy the supplied predicate"""
+        where_table = Table(self.columns)
+        where_table.rows = filter(predicate, self.rows)
+        return where_table
+
+    def limit(self, num_rows):
+        """return only the first num_rows rows"""
+        limit_table = Table(self.columns)
+        limit_table.rows = self.rows[:num_rows]
+        return limit_table
+
+    
+    
 
 
 
